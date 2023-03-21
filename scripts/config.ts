a50c88ecfc2b64ac00bd73709ea82ba4ce347b2b
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-03-21 10:26:39 +0530

    Port config to typescript

---
+++ scripts/config.ts
@@ -0,0 +1,11 @@
+export const STOPPED_PREFIX = '[yellow]\u26A0[scarlet]Marked Griefer[yellow]\u26A0[white]';
+export const ADMIN_PREFIX = '[black]<[scarlet]A[black]>';
+export const MOD_PREFIX = '[black]<[green]M[black]>';
+export const AFK_PREFIX = '[orange]\uE876 AFK \uE876 | [white]';
+export const MUTED_PREFIX = '[white](muted)';
+export const MEMBER_PREFIX = '[black]<[yellow]\uE809[black]>[white]';
+export const bannedWords = ['nigger', 'kill yourself', 'kill urself', 'kys', 'cock', 'cock sucker'];
+export const bannedNames = ['goldberg', 'eshay'];
+
+// const ip = 'localhost';
+export const ip = '45.79.202.111';
