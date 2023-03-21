29d4454448001fc40d2f135ed70368056687c8d5
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-03-21 18:59:44 +0530

    Add dummy main.js

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+/**
+ * Main file for Fish Commands.
+ * Does not do anything other than requiring index.js.
+ */
+
+importPackage(Packages.arc);
+importPackage(Packages.mindustry.type);
+
+require("index");
