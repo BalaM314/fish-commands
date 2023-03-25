363fce7d4120a4ac7207e959e7bfc265eebfaadc
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-03-25 13:44:32 +0530

    oops, need to polyfill fromEntries

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,8 @@ importPackage(Packages.mindustry.type);
 //Polyfills
 Object.entries = o => Object.keys(o).map(k => [k, o[k]]);
 Object.values = o => Object.keys(o).map(k => o[k]);
+Object.fromEntries = a => a.reduce((o, [k, v]) => { o[k] = v; return o; }, {});
+//Arrow functions do not bind to "this"
 Array.prototype.at = function(i){
 	return this[i < 0 ? this.length + i : i];
 }
