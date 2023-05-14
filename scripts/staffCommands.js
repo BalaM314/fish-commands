79c9a5ea64e6244c441bbdfbc7908a5a47fa60e5
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-05-14 18:36:09 +0530

    Fix crash in logging stop_offline part 2

--- scripts/staffCommands.js
+++ scripts/staffCommands.js
@@ -191,7 +191,7 @@ exports.commands = __assign(__assign({ warn: {
                 var fishP = players_1.FishPlayer.getFromInfo(option);
                 if (sender.canModerate(fishP, true)) {
                     fishP.stop(sender);
-                    (0, utils_1.logAction)('stopped', sender, fishP);
+                    (0, utils_1.logAction)('stopped', sender, option);
                     outputSuccess("Player \"".concat(option.lastName, "\" was stopped."));
                 }
                 else {
