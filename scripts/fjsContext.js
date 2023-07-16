adcae66f2cd87e667cc0759945a6838cae067f4e
BalaM314 <71201189+BalaM314@users.noreply.github.com>
2023-07-16 18:38:19 +0530

    stupidity

--- scripts/fjsContext.js
+++ scripts/fjsContext.js
@@ -16,7 +16,7 @@ const utils = require("./utils");
 
 const { Perm, allCommands } = commands;
 const { FishPlayer } = players;
-const { Ranks, RoleFlag } = ranks;
+const { Rank, RoleFlag } = ranks;
 const { menu } = menus;
 const { Ohnos } = ohno;
 Object.assign(this, utils); //global scope goes brrrrr, I'm sure this will not cause any bugs whatsoever
