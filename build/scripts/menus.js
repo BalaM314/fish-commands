519362dcd01a4beac432c648001bd30a91690a35
juror #9 <64419198+Jurorno9@users.noreply.github.com>
2025-01-25 10:50:01 -0500

    fixed GUI_Cancel

--- build/scripts/menus.js
+++ build/scripts/menus.js
@@ -40,7 +40,7 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
     return to.concat(ar || Array.prototype.slice.call(from));
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.menu = exports.listeners = exports.GUI_Confirm = exports.GUI_Page = exports.GUI_Cancel = exports.GUI_Container = exports.registerListeners = void 0;
+exports.menu = exports.listeners = exports.GUI_Confirm = exports.GUI_Page = exports.GUI_Cancel = exports.GUI_Container = exports.listMenu = exports.pageMenu = exports.registerListeners = void 0;
 var commands_1 = require("./commands");
 var players_1 = require("./players");
 var utils_1 = require("./utils");
@@ -89,14 +89,11 @@ function registerListeners() {
 exports.registerListeners = registerListeners;
 //this is a minor abomination but theres no good way to do overloads in typescript
 function menu(title, description, elements, target, callback) {
-    target.activeMenu.cancelOptionId = -1;
+    //target.activeMenu.cancelOptionId = -1; GUI_Cancel handles cancel already
     var ArrangedElements = { data: [], stringified: [] };
     elements.forEach(function (element) {
         var _a;
         (_a = ArrangedElements.data).push.apply(_a, __spreadArray([], __read(element.data()), false));
-        if (element instanceof GUI_Cancel) {
-            target.activeMenu.cancelOptionId = ArrangedElements.data.length;
-        }
     });
     elements.forEach(function (element) {
         var _a;
@@ -121,7 +118,6 @@ function menu(title, description, elements, target, callback) {
             //which already checks permissions.
             //Additionally, the callback is cleared by the generic menu listener after it is executed.
             //We do need to validate option though, as it can be any number.
-            Log.info("Option ".concat(option, " in ").concat(PackedElements.data.length));
             if (!(option in PackedElements.data))
                 return;
             if (typeof PackedElements.data[option] === 'string' && PackedElements.data[option] == "cancel") {
@@ -182,6 +178,7 @@ function pageMenu(title, description, elements, target, callback) {
         return;
     }
 }
+exports.pageMenu = pageMenu;
 //auto formats a array into a page menu
 //TODO make list a GUI_Element[] instead of a single Container
 function listMenu(title, description, list, target, callback, pageSize) {
@@ -194,6 +191,7 @@ function listMenu(title, description, list, target, callback, pageSize) {
     buttons.data.forEach(function (page) { pages.push([new GUI_Container(page, 1, list.stringifier)]); }); //wrap each page in a container
     pageMenu(title, description, pages, target, callback);
 }
+exports.listMenu = listMenu;
 //const reservedStrings = ["left", "center", "right"] // strings used for paged menus, cannot be handled correct
 var GUI_Container = /** @class */ (function () {
     function GUI_Container(options, columns, stringifier) {
